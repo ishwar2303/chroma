36ec96efe3c5dae59923ae2b1ea1a669807720a3
ishwar2303 <ishwar2303@gmail.com>
2021-03-17 22:21:55 +0530

    updated

--- pretty-code-assets/script/language-setting.js
+++ pretty-code-assets/script/language-setting.js
@@ -1,19 +1,14 @@
 let language
-export default language = [
-    {
-        lang : 'html',
-        imported : false
+export default language = [{
+        lang: 'html',
+        imported: false
     },
     {
-        lang : 'css',
-        imported : false
+        lang: 'css',
+        imported: false
     },
     {
-        lang : 'javascript',
-        imported : false
-    },
-    {
-        lang : 'php',
-        imported : false
+        lang: 'javascript',
+        imported: false
     }
 ]
--- pretty-code-assets/script/main.js
+++ pretty-code-assets/script/main.js
@@ -6,20 +6,20 @@ let body = document.getElementsByTagName('body')[0]
 let head = document.getElementsByTagName('head')[0]
 console.log(prettyCodeContainer)
 
-function import_required_assets(){
+function import_required_assets() {
     let i, j, textarea, message
-    let dataLanguage, link, userCodeContainer 
+    let dataLanguage, link, userCodeContainer
     let languageCssPath = 'pretty-code-assets/css/language/'
     let languageSupported
-    // console.log(language)
-    for(i=0; i<prettyCodeContainer.length; i++){
+        // console.log(language)
+    for (i = 0; i < prettyCodeContainer.length; i++) {
         console.log(prettyCodeContainer[i])
         dataLanguage = prettyCodeContainer[i].attributes[1].nodeValue
         languageSupported = false
-        for(j=0; j<language.length; j++){
-            if(language[j].lang == dataLanguage)
+        for (j = 0; j < language.length; j++) {
+            if (language[j].lang == dataLanguage)
                 languageSupported = true
-            if(language[j].lang == dataLanguage && !language[j].imported){
+            if (language[j].lang == dataLanguage && !language[j].imported) {
                 language[j].imported = true
                 link = document.createElement('link')
                 link.href = languageCssPath + dataLanguage + '.css'
@@ -28,28 +28,26 @@ function import_required_assets(){
                 break;
             }
         }
-        if(languageSupported){
-            userCodeContainer = prettyCodeContainer[i].getElementsByClassName('pretty-user-code')[0] 
-            textarea = prettyCodeContainer[i].getElementsByTagName('textarea')[0] 
-            if(textarea){
+        if (languageSupported) {
+            userCodeContainer = prettyCodeContainer[i].getElementsByClassName('pretty-user-code')[0]
+            textarea = prettyCodeContainer[i].getElementsByTagName('textarea')[0]
+            if (textarea) {
                 console.log(textarea)
-                // console.log(userCodeContainer)
+                    // console.log(userCodeContainer)
                 convertToPrettyCode(prettyCodeContainer[i], userCodeContainer, dataLanguage, textarea.value)
                 textarea.remove()
-            }
-            else{
+            } else {
                 console.error('textarea with corresponding code-wrapper not found')
                 message = 'th'
-                if(i == 0)
+                if (i == 0)
                     message = 'st'
-                else if(i == 1)
+                else if (i == 1)
                     message = 'nd'
-                else if(i == 2)
+                else if (i == 2)
                     message = '3rd'
-                console.error('Check ' + (i+1) + message + ' position code-wrapper in your code')
+                console.error('Check ' + (i + 1) + message + ' position code-wrapper in your code')
             }
-        }
-        else{
+        } else {
             console.error('Error : ' + dataLanguage + ' not supported')
             console.error('Check your spelling in data-lang attribute')
             console.error('check supported language from conversion.js')
